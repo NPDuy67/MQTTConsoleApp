7a94989874e8da227a63a121d179e695ab9982ea
NPDuy67 <111833170+NPDuy67@users.noreply.github.com>
2022-08-23 16:32:52 +0700

    Add files via upload

---
+++ App.cs
@@ -0,0 +1,46 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Text;
+
+namespace MQTTSubscriber
+{
+    class App
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            var client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                            .WithClientId(Guid.NewGuid().ToString())
+                            .WithTcpServer("test.mosquitto.org", 1883)
+                            .WithCleanSession()
+                            .Build();
+            client.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("Connected Successful");
+                var toppicFilter = new TopicFilterBuilder()
+                                        .WithTopic("Sensor")
+                                        .Build();
+                await client.SubscribeAsync(toppicFilter);
+            });
+
+            client.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Disconnected");
+            });
+
+            client.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine($"{Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            });
+
+            await client.ConnectAsync(options);
+
+            Console.ReadLine();
+
+            await client.DisconnectAsync();
+        }
+    }
+}
